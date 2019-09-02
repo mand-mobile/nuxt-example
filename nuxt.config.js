996bfe5c4b0b96ca2584d075d0c1c44f4200819f
xuxiaoyan <xuxiaoyan@didiglobal.com>
2019-09-02 14:45:59 +0800

    upgrade nuxt

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,26 +1,18 @@
 const fs = require('fs')
 const path = require('path')
-const pkg = require('./package')
-
-// Avoid css files being executed
-require.extensions['.css'] = () => {
-  return
-}
 
 const responsiveJS = fs.readFileSync(path.resolve(__dirname, './plugins/responsive.js'), 'utf8')
-
-module.exports = {
+export default {
   mode: 'universal',
-
   /*
   ** Headers of the page
   */
   head: {
-    title: pkg.name,
+    title: process.env.npm_package_name || '',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: pkg.description }
+      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
@@ -30,51 +22,36 @@ module.exports = {
     ],
     __dangerouslyDisableSanitizers: ['script']
   },
-
   /*
   ** Customize the progress-bar color
   */
   loading: { color: '#fff' },
-
   /*
   ** Global CSS
   */
   css: [
     'mand-mobile/lib/mand-mobile.css',
-    '@/assets/global.css',
   ],
-
   /*
   ** Plugins to load before mounting the App
   */
   plugins: [
-    // { src: '~/plugins/responsive', ssr: false }
+    // { src: '~/plugins/reset.js' },
   ],
-
+  /*
+  ** Nuxt.js dev-modules
+  */
+  buildModules: [
+  ],
   /*
   ** Nuxt.js modules
   */
   modules: [
   ],
-
   /*
   ** Build configuration
   */
   build: {
-    babel: {
-      presets: [
-        '@vue/app'
-      ],
-      // plugins: [
-      //   [
-      //     'import',
-      //     {
-      //       libraryName: 'mand-mobile',
-      //       libraryDirectory: 'lib'
-      //     }
-      //   ]
-      // ]
-    },
     postcss: {
       plugins: {
         'autoprefixer': {},
@@ -88,16 +65,7 @@ module.exports = {
     /*
     ** You can extend webpack config here
     */
-    extend(config, ctx) {
-      // Run ESLint on save
-      if (ctx.isDev && ctx.isClient) {
-        config.module.rules.push({
-          enforce: 'pre',
-          test: /\.(js|vue)$/,
-          loader: 'eslint-loader',
-          exclude: /(node_modules)/
-        })
-      }
+    extend (config, ctx) {
     }
   }
 }
